9b0a0563c1d71724fcd8d1ff76009ced0ca9f6a9
Sam Craig <contact@samcraig.io>
2020-08-01 16:27:36 -0400

    chore(gatsby): removed prop artifact from sensors library conversion

--- packages/gatsby/src/ts/index.ts
+++ packages/gatsby/src/ts/index.ts
@@ -1,11 +1,1 @@
-import * as React from "react";
-
-/**
- * Default sensor attributes.
- */
-export interface ISensorProps {
-	/**
-	 * The time in milliseconds to throttle invocations of the corresponding sensor.
-	 */
-	throttle: number;
-}
+export {};
