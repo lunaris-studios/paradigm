17dc636b7d9773856c7bf116d5db57f1aa8b03aa
Sam Craig <contact@samcraig.io>
2020-08-06 19:35:45 -0400

    feat(protocol): added stage enum

---
+++ packages/protocol/src/constants/_stage.ts
@@ -0,0 +1,8 @@
+/**
+ * State of the active code environment, paralleled by
+ * defaults for `process.env.NODE_ENV`
+ */
+export enum Stage {
+	PRODUCTION = "production",
+	DEVELOPMENT = "development",
+}
