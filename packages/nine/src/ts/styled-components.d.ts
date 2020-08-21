60c5afc614e52859e7b92f2c700f844a364e523b
Sam Craig <contact@samcraig.io>
2020-08-21 09:25:15 -0400

    docs(nine): add tsdoc annotation for styled-component DefaultTheme interface

--- packages/nine/src/ts/styled-components.d.ts
+++ packages/nine/src/ts/styled-components.d.ts
@@ -3,5 +3,11 @@ import * as Protocol from "@paradigmjs/protocol";
 import "styled-components";
 
 declare module "styled-components" {
+	/**
+	 * Intended declaration merge for extension of the root theme
+	 * in all `styled-components`.
+	 *
+	 * https://styled-components.com/docs/api#create-a-declarations-file
+	 */
 	export interface DefaultTheme extends Protocol.Theme {}
 }
