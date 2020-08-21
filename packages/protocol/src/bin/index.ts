6d8a965ce5cd268388a87d16e75d7f673e46858d
Sam Craig <contact@samcraig.io>
2020-08-21 09:07:01 -0400

    style(protocol): remove bin folder, moved exports to root

--- packages/protocol/src/bin/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./_bind";
-export * from "./_theme";
-export * from "./_typography";
-export * from "./_typography-style";
--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,5 +1,9 @@
 export * from "./constants";
-export * from "./bin";
+
+export * from "./_bind";
+export * from "./_theme";
+export * from "./_typography";
+export * from "./_typography-style";
 
 import * as Mixins from "./mixins";
 import * as Snippets from "./snippets";
