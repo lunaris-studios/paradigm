356158fcc4fd9195c0a22135fedb94f87767659c
Sam Craig <contact@samcraig.io>
2020-08-22 15:15:21 -0400

    fix: add lodash-es moduleNameMapper override for jest config

--- packages/nine/jest.config.js
+++ packages/nine/jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
 		".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
 			"<rootDir>/mocks/file-mock.js",
 		"^~/(.*)$": "<rootDir>/src/$1",
+		"^lodash-es$": "lodash",
 	},
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 	testPathIgnorePatterns: ["node_modules", ".cache"],
