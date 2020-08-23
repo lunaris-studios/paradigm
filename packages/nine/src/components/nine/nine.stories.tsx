53dcc67c0fbbab37397dbcfb88bfba9a4baf1e7a
Sam Craig <contact@samcraig.io>
2020-08-22 21:24:36 -0400

    test(nine): add basic, vertical, horizontal, big, content stories

--- packages/nine/src/components/nine/nine.stories.tsx
+++ packages/nine/src/components/nine/nine.stories.tsx
@@ -4,12 +4,47 @@ import * as Util from "~/util";
 
 import * as Component from "./nine";
 
+import BORDER from "~/assets/border.png";
+import SWORD from "~/assets/sword.png";
+
 const Template: Util.Storybook.React.Story<Component.INineProps> = (args) => (
-	<Component.Nine {...args} />
+	<Component.Nine {...args} image={BORDER} corner={22} />
 );
 
 export const Basic = Template.bind({});
 Basic.args = {};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+	height: 256,
+	width: 128,
+};
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+	height: 128,
+	width: 256,
+};
+
+export const Big = Template.bind({});
+Big.args = {
+	height: 512,
+	width: 512,
+};
+
+export const Content = Template.bind({});
+Content.args = {
+	children: (
+		<img
+			src={SWORD}
+			style={{
+				height: "100%",
+				width: "100%",
+				objectFit: "contain",
+			}}
+		/>
+	),
+};
 
 export default {
 	title: "Nine",
