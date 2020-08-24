dfd6054d1b9e48a74c8a79ea634b8c923d8f8629
Sam Craig <contact@samcraig.io>
2020-08-24 18:42:16 -0400

    fix(util): remove protocol package to fix circular dependencies

--- packages/util/src/util/_js-util.ts
+++ packages/util/src/util/_js-util.ts
@@ -1,12 +1,10 @@
-import * as Protocol from "@paradigmjs/protocol";
-
 // only accessible within this file, so use `Utils.isNodeEnv(env)` from the outside.
 declare var process: { env: any };
 
 /**
  * Returns whether `process.env.NODE_ENV` exists and equals `env`.
  */
-export function isNodeEnv(env: Protocol.Stage) {
+export function isNodeEnv(env: string) {
 	return typeof process !== "undefined" && process.env && process.env.NODE_ENV === env;
 }
 
