321a785224f29eee920d6288e3a631425c344d3c
Sam Craig <contact@samcraig.io>
2020-08-24 18:44:52 -0400

    feat(protocol): add debug snippets

--- packages/protocol/src/snippets/_debug.ts
+++ packages/protocol/src/snippets/_debug.ts
@@ -1,12 +1,20 @@
 import * as SC from "styled-components";
+// re-import
+import styled, { css } from "styled-components";
 
-import { css } from "styled-components";
+import * as Util from "~/util";
+
+import { Stage } from "../constants/_stage";
 
 /**
- * Non-intrusive, bold border used to debug bounding boxes for elements.
+ * Non-intrusive, bold border used to debug bounding boxes for elements. We also check
+ * that the current `process.env.NODE_ENV` is equal to `development` so this never
+ * never hits a staging / production build.
  */
 export function debug(): SC.FlattenSimpleInterpolation {
-	return css`
-		border: 1px solid red;
-	`;
+	return Util.isNodeEnv(Stage.DEVELOPMENT)
+		? css`
+				border: 1px solid red;
+		  `
+		: css``;
 }
