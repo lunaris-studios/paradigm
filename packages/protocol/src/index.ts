eaa698a56bcd5f444ae26efc04358efd6bda1bb6
Sam Craig <contact@samcraig.io>
2020-08-27 14:45:42 -0400

    style(protocol): rearrange barrel file

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,9 +1,10 @@
-export * from "./constants";
-
 export * from "./_bind";
 export * from "./_theme";
 export * from "./_typography";
 export * from "./_typography-style";
+
+export * from "./constants";
+export * from "./ts";
 
 import * as Mixins from "./mixins";
 import * as Snippets from "./snippets";
