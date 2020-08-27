3391dee3c771a23504d15c0d495b4833466f9f7a
Sam Craig <contact@samcraig.io>
2020-08-27 14:46:24 -0400

    chore(): remove local file extension module defintions in lieu of @paradigmjs/util/dist/ts/misc/extensions

--- packages/core/src/ts/global.d.ts
+++ packages/core/src/ts/global.d.ts
@@ -1,42 +1,8 @@
-/**
- * Fonts
- */
-declare module "*.woff2" {
-	const src: string;
-	export = src;
-}
-
-declare module "*.woff" {
-	const src: string;
-	export = src;
-}
-
-declare module "*.eot" {
-	const src: string;
-	export = src;
-}
-
-declare module "*.ttf" {
-	const src: string;
-	export = src;
-}
-
-declare module "*.otf" {
-	const src: string;
-	export = src;
-}
-
-/**
- * Images
- */
-declare module "*.svg" {
-	const src: string;
-	const ReactComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-	export { ReactComponent };
-	export = src;
-}
-
-declare module "*.png" {
-	const src: string;
-	export = src;
-}
+import "@paradigmjs/protocol/dist/ts/styled-components";
+import "@paradigmjs/util/dist/ts/misc/extensions";
+import "@paradigmjs/util/dist/ts/styled-components/test-utils";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import "jest-styled-components";
+import "jest-extended";
+import "styled-components/cssprop";
--- packages/core/src/ts/import.d.ts
+++
@@ -1,5 +0,0 @@
-import "@paradigmjs/util/dist/ts/declarations/styled-components/test-utils";
-import "@testing-library/jest-dom/extend-expect";
-import "@testing-library/jest-dom";
-import "jest-styled-components";
-import "jest-extended";
